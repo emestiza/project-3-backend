2e7555187c8687df91f9658cdb78bc69c7e26e4e
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2020-08-25 14:52:51 -0400

    user get route done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
+  get "/users/:username", to: "users#show"
 
 
   resources :budgets do
